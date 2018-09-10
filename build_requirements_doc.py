d3d6855a64b21e6aef2aed153c2a2b123d5cc4c6
Daniel Phillips <dphildev@gmail.com>
2018-09-10 17:29:06 +0100

    Tweaking to use new Requirement tickets instead of Epics.

--- build_requirements_doc.py
+++ build_requirements_doc.py
@@ -1,42 +1,56 @@
 from jira import JIRA
 import pystache
 from datetime import datetime
+import json
 
 options = { 'server': 'http://shaipjira.local.tmvse.com' }
 jira = JIRA(options)
 project = jira.project('10002')
-epics = jira.search_issues(
-    'project = SHAIP AND issuetype = Epic AND resolution = Unresolved ORDER BY id ASC',
+requirements = jira.search_issues(
+    'project = SHAIP AND issuetype = Requirement AND resolution = Unresolved ORDER BY id ASC',
     expand="renderedFields")
 
-template_epics = []
-for epic in epics:
-    epic_children = jira.search_issues('"Epic Link" = {}'.format(epic.key))
+template_requirements = []
+for requirement in requirements:
+    links = []
+    for link in requirement.fields.issuelinks:
+        if link.type.name != "Defines":
+            continue
 
-    children = []
-    for child in epic_children:
-        children.append({
-            'key': child.key,
-            'summary': child.fields.summary,
-            'icon_type': child.fields.issuetype.iconUrl,
-            'permalink': child.permalink(),
-            'closed': child.fields.resolution is not None
+        link_type = None
+        issue = None
+        if hasattr(link, 'outwardIssue'):
+            link_type = "defines"
+            issue = link.outwardIssue
+        else:
+            link_type = "is defined by"
+            issue = link.inwardIssue
+
+        links.append({
+            'key': issue.key,
+            'summary': issue.fields.summary,
+            'icon_type': issue.fields.issuetype.iconUrl,
+            'permalink': issue.permalink(),
+            'closed': issue.fields.status.name == "Done",
+            'link_type': link_type
         })
 
-    template_epics.append({
-        'key': epic.key,
-        'summary': epic.fields.summary,
-        'description': epic.renderedFields.description,
-        'children': children,
-        'permalink': epic.permalink()
+    template_requirements.append({
+        'key': requirement.key,
+        'summary': requirement.fields.summary,
+        'description': requirement.renderedFields.description,
+        'links': links,
+        'permalink': requirement.permalink(),
+        'labels': requirement.fields.labels
     })
 
 renderer = pystache.Renderer()
 context = {
     'project_name': project.name,
-    'epics': template_epics,
+    'requirements': template_requirements,
     'time': datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 }
 
 with open('output.html', 'w') as file:
+    #print(json.dumps(context, sort_keys=True, indent=4))
     file.write(renderer.render_path("templates/requirements.html.mustache", context))
